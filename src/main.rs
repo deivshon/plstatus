46b92bb2d0a5207ec2cc5e64ea78f8be81f96c94
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-09-24 16:40:09 +0200

    feat: :sparkles: add graceful termination handler

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 
 use std::{
     env,
-    process::Command,
+    process::{self, Command},
     sync::{Arc, Mutex},
     thread,
     time::Duration,
@@ -15,36 +15,46 @@ use component::Component;
 use config::{Config, DEFAULT_CONFIG_PATH};
 use utils::{failure, warn};
 
+fn print_status(status: &str) {
+    let print_status = Command::new("xsetroot").arg("-name").arg(status).status();
+
+    if let Err(e) = print_status {
+        warn(format!("Could not print status on bar: {}", e))
+    }
+}
+
+fn termination_handler(status: &Arc<Mutex<String>>) {
+    let _status = status.lock().unwrap();
+    print_status("");
+    process::exit(1);
+}
+
 fn status_loop(status: Arc<Mutex<String>>, config: Config) {
     if let Some(wait) = config.first_wait {
         thread::sleep(Duration::from_millis(wait));
     }
 
-    let mut status = status.lock().unwrap();
     loop {
-        (*status).clear();
-        for (idx, c) in (&config.components).iter().enumerate() {
-            let component = c.lock().unwrap();
-            *status = format!(
-                "{}{}{}{}",
-                component.label,
-                *status,
-                component.current_result,
-                if idx != config.components.len() - 1 && !component.current_result.is_empty() {
-                    component.separator.as_str()
-                } else {
-                    ""
-                },
-            );
-        }
+        {
+            let mut status = status.lock().unwrap();
 
-        let print_status = Command::new("xsetroot")
-            .arg("-name")
-            .arg((*status).as_str())
-            .status();
+            (*status).clear();
+            for (idx, c) in (&config.components).iter().enumerate() {
+                let component = c.lock().unwrap();
+                *status = format!(
+                    "{}{}{}{}",
+                    component.label,
+                    *status,
+                    component.current_result,
+                    if idx != config.components.len() - 1 && !component.current_result.is_empty() {
+                        component.separator.as_str()
+                    } else {
+                        ""
+                    },
+                );
+            }
 
-        if let Err(e) = print_status {
-            warn(format!("Could not print status on bar: {}", e))
+            print_status((*status).as_str());
         }
 
         thread::sleep(Duration::from_millis(config.period));
@@ -86,6 +96,9 @@ fn main() {
         thread::spawn(move || Component::main_loop(&c_ref));
     }
 
+    let status_ref = Arc::clone(&status);
+    ctrlc::set_handler(move || termination_handler(&status_ref))
+        .expect("Could not set termination handler");
     let status_loop_handle = thread::spawn(move || status_loop(status, config));
 
     status_loop_handle.join().unwrap();
