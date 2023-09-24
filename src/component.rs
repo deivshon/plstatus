fb4d605281da26542020fc5db842838288a47d20
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-09-24 11:26:30 +0200

    feat: :sparkles: implement basic working example

---
+++ src/component.rs
@@ -0,0 +1,63 @@
+use std::{
+    process::Command,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+pub struct Component {
+    pub binary: String,
+    pub args: Vec<String>,
+    pub period: Option<u64>,
+    pub current_result: String,
+}
+
+impl Component {
+    pub fn new(binary: String, args: Vec<String>, period: Option<u64>) -> Self {
+        return Component {
+            binary,
+            args,
+            period,
+            current_result: String::new(),
+        };
+    }
+
+    pub fn main_loop(component_ref: &Arc<Mutex<Self>>) {
+        let period;
+        {
+            let component = component_ref.lock().unwrap();
+            period = component.period;
+        }
+
+        loop {
+            Self::run(component_ref);
+
+            match period {
+                Some(p) => thread::sleep(Duration::from_millis(p)),
+                None => break,
+            }
+        }
+    }
+
+    fn run(component_ref: &Arc<Mutex<Self>>) {
+        let mut command;
+        {
+            let component = component_ref.lock().unwrap();
+            command = Command::new(&component.binary);
+            let mut arged_command = &mut command;
+            for string in &component.args {
+                arged_command = arged_command.arg(string);
+            }
+        }
+
+        let output = match command.output() {
+            Ok(o) => String::from_utf8_lossy(&o.stdout).trim().to_string(),
+            Err(_) => String::from("Could not obtain output"),
+        };
+
+        {
+            let mut component = component_ref.lock().unwrap();
+            component.current_result = output;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+mod component;
+
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+use component::Component;
+
+fn status_loop(status: Arc<Mutex<String>>, components: Vec<Arc<Mutex<Component>>>) {
+    let mut status = status.lock().unwrap();
+    loop {
+        *status = String::from("");
+        for c in &components {
+            let component = c.lock().unwrap();
+            *status = format!("{}|{}", *status, component.current_result);
+        }
+        println!("{}", status);
+
+        thread::sleep(Duration::from_millis(25));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let status = Arc::new(Mutex::new(String::new()));
+    let a = Arc::new(Mutex::new(Component::new(
+        String::from("date"),
+        vec![String::from("+%3N")],
+        Some(100),
+    )));
+
+    let b = Arc::new(Mutex::new(Component::new(
+        String::from("date"),
+        vec![String::from("+%3N")],
+        Some(50),
+    )));
+
+    let a_clone = Arc::clone(&a);
+    let _a_handle = thread::spawn(move || Component::main_loop(&a_clone));
+
+    let b_clone = Arc::clone(&b);
+    let _b_handle = thread::spawn(move || Component::main_loop(&b_clone));
+
+    let components_refs = vec![Arc::clone(&a), Arc::clone(&b)];
+    let status_loop_handle = thread::spawn(|| status_loop(status, components_refs));
+
+    status_loop_handle.join().unwrap();
 }
